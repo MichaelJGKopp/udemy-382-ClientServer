ba3ac45edd61222788a22ad0f9d34f1f4f021f45
Michael Kopp <MichaelJG.Kopp@gmail.com>
2024-11-06 19:22:08 +0100

    observe state of buffer after each operation

---
+++ src/dev/lpa/Main.java
@@ -0,0 +1,23 @@
+package dev.lpa;
+
+import java.nio.ByteBuffer;
+import java.util.function.Consumer;
+
+public class Main {
+  
+  public static void main(String[] args) {
+  
+  
+  }
+  
+  private static void doOperation(String op, ByteBuffer buffer, Consumer<ByteBuffer> c) {
+    System.out.printf("%-30s", op);
+    c.accept(buffer);
+    System.out.printf("Capacity = %d, Limit = %d, Position = %d, Remaining = %d%n",
+      buffer.capacity(),
+      buffer.limit(),
+      buffer.position(),
+      buffer.remaining()
+    );
+  }
+}
